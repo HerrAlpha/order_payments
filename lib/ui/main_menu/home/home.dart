44c67decffbbe7ac6e11e46072b32e8545975391
wildanromiza <38659848+wildanelek12@users.noreply.github.com>
2023-04-04 22:13:23 +0700

    change layout

--- lib/ui/main_menu/home/home.dart
+++ lib/ui/main_menu/home/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:order_payments/bloc/product/product_cubit.dart';
+import 'package:order_payments/model/product.dart';
 import 'package:order_payments/utils/constant.dart' as Constants;
 
 class Home extends StatefulWidget {
@@ -129,39 +130,61 @@ class _HomeState extends State<Home> {
                     fit: FlexFit.tight,
                     child: BlocBuilder<ProductCubit, ProductState>(
                         builder: (context, state) {
-                      if (state is ProductInitialState ||
-                          state is ProductLoadingState) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      } else if (state is ProductResponseState) {
-                        final products = state.product;
-                        return ListView.builder(
-                            controller: scrollController,
-                            itemCount: products.length,
-                            itemBuilder: (context, index) {
-                              final product = products[index];
-                              if (index < products.length) {
-                                return ListTile(
-                                  leading: Image.network('${product.thumbnail}',
-                                      width: 100, height: 100),
-                                  title: Text(product.title.toString()),
-                                );
-                              } else {
-                                Timer(Duration(milliseconds: 30), () {
-                                  scrollController.jumpTo(scrollController
-                                      .position.maxScrollExtent);
-                                });
-
-                                return Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              }
-                            });
+                      if (state is ProductLoadingState && state.isFirstFetch) {
+                        return _loadingIndicator();
                       }
-                      return Center(
-                        child: Text(state.toString()),
+
+                      List<Products> products = [];
+                      bool isLoading = false;
+                      if (state is ProductLoadingState) {
+                        products = state.oldProduct;
+                        isLoading = true;
+                      } else if (state is ProductResponseState) {
+                        products = state.product;
+                      }
+
+                      return GridView.builder(
+                        controller: scrollController,
+                        gridDelegate:
+                            const SliverGridDelegateWithMaxCrossAxisExtent(
+                                maxCrossAxisExtent: 200,
+                                childAspectRatio: 1,
+                                crossAxisSpacing: 10,
+                                mainAxisSpacing: 20),
+                        itemBuilder: (context, index) {
+                          if (index < products.length)
+                            return _product(products[index], context);
+                          else {
+                            Timer(Duration(milliseconds: 30), () {
+                              scrollController.jumpTo(
+                                  scrollController.position.maxScrollExtent);
+                            });
+                            return _loadingIndicator();
+                          }
+                        },
+                        itemCount: products.length + (isLoading ? 1 : 0),
                       );
+
+                      // return ListView.separated(
+                      //   controller: scrollController,
+                      //   itemBuilder: (context, index) {
+                      //     if (index < products.length)
+                      //       return _product(products[index], context);
+                      //     else {
+                      //       Timer(Duration(milliseconds: 30), () {
+                      //         scrollController.jumpTo(
+                      //             scrollController.position.maxScrollExtent);
+                      //       });
+                      //       return _loadingIndicator();
+                      //     }
+                      //   },
+                      //   separatorBuilder: (context, index) {
+                      //     return Divider(
+                      //       color: Colors.grey[400],
+                      //     );
+                      //   },
+                      //
+                      // );
                     }),
                   )
                 ],
@@ -169,6 +192,40 @@ class _HomeState extends State<Home> {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _loadingIndicator() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(child: CircularProgressIndicator()),
+    );
+  }
+
+  Widget _product(Products product, BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, //add this
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Image.network(
+                  product.thumbnail,
+                  fit: BoxFit.fill, // add this
+                ),
+              ),
+            ),
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(product.title),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
